7245fd025042028cfaf7fcbe9fde3c8ff626292c
PhiloTheePhilix <110274378+Philotheephilix@users.noreply.github.com>
2024-04-05 15:18:08 +0530

    initial push

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, redirect, render_template, jsonify,request, url_for
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/homepage')
+def homepage():
+    return render_template('frame-9.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
