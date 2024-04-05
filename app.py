a1e49e80490b29ea9090f60e790b90d15175c53a
JohannaFernando <166120409+JohannaFernando@users.noreply.github.com>
2024-04-05 18:41:09 +0530

    Profile updated

--- app.py
+++ app.py
@@ -10,5 +10,12 @@ def index():
 def homepage():
     return render_template('frame-9.html')
 
+
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
